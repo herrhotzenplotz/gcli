accb6cab36f5502f6a0cf1111c199ce6fb8665ff
Nico Sonack <nsonack@herrhotzenplotz.de>
2022-03-22 16:39:24 +0100

    (#114) Cache color escape sequences

--- src/color.c
+++ src/color.c
@@ -30,17 +30,63 @@
 #include <ghcli/color.h>
 #include <ghcli/config.h>
 
-const char *
-ghcli_setcolor(uint32_t colorcode)
+#include <stdlib.h>
+
+static struct {
+    uint32_t code;
+    char *sequence;
+} color_table[1024];
+static size_t color_table_size;
+
+static void
+clean_color_table(void)
 {
+    for (size_t i = 0; i < color_table_size; ++i)
+        free(color_table[i].sequence);
+}
+
+static char *
+color_cache_lookup(uint32_t code)
+{
+    for (size_t i = 0; i < color_table_size; ++i) {
+        if (color_table[i].code == code)
+            return color_table[i].sequence;
+    }
+    return NULL;
+}
+
+static void
+color_cache_insert(uint32_t code, char *sequence)
+{
+    color_table[color_table_size].code     = code;
+    color_table[color_table_size].sequence = sequence;
+    color_table_size++;
+}
+
+const char *
+ghcli_setcolor(uint32_t code)
+{
+    char *result = NULL;
+
     if (!ghcli_config_have_colors())
         return "";
 
+    if (color_table_size == 0)
+        atexit(clean_color_table);
+
+    result = color_cache_lookup(code);
+    if (result)
+        return result;
+
     /* TODO: This is inherently screwed */
-    return sn_asprintf("\033[38;2;%02d;%02d;%02dm",
-                       (colorcode & 0xFF000000) >> 24,
-                       (colorcode & 0x00FF0000) >> 16,
-                       (colorcode & 0x0000FF00) >>  8);
+    result = sn_asprintf("\033[38;2;%02d;%02d;%02dm",
+                         (code & 0xFF000000) >> 24,
+                         (code & 0x00FF0000) >> 16,
+                         (code & 0x0000FF00) >>  8);
+
+    color_cache_insert(code, result);
+
+    return result;
 }
 
 const char *

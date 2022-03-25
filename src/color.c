f2e50c18ff14836734a793218299ca0263bc44c8
Nico Sonack <nsonack@herrhotzenplotz.de>
2022-03-25 18:03:59 +0100

    Colorize reviews

--- src/color.c
+++ src/color.c
@@ -159,6 +159,12 @@ ghcli_state_color_sv(sn_sv state)
 
         if (sn_sv_has_prefix(state, "closed"))
             return ghcli_setcolor(GHCLI_COLOR_RED);
+
+        if (sn_sv_has_prefix(state, "COMMENTED"))
+            return ghcli_setcolor(GHCLI_COLOR_BLUE);
+
+        if (sn_sv_has_prefix(state, "APPROVED"))
+            return ghcli_setcolor(GHCLI_COLOR_GREEN);
     }
 
     return ghcli_setcolor(GHCLI_COLOR_DEFAULT);

2adea9ce5a39dbb4457ebf3c684763e20e8bdc51
Nico Sonack <nsonack@herrhotzenplotz.de>
2022-10-01 19:52:06 +0200

    Rewrite gentarballs script to use git archive

--- tools/gentarball.sh
+++ tools/gentarball.sh
@@ -1,73 +1,31 @@
-#!/opt/schily/bin/bosh
-
-die() {
-	printf -- "error: %s\n" "$*" 1>&2
-	exit 1
-}
-
-info() {
-	printf -- "info: %s\n" "$*" 1>&2
-}
+#!/bin/sh
 
 findversion() {
-	grep '^GCLI_VERSION' Makefile \
-		 | head -1 \
-		 | awk -F\= '{print $2}' \
-		 | sed -E 's/[[:space:]]*([^[:space:]]*)[[:space:]]*$/\1/g'
+	grep 'GCLI_VERSION' build.sh \
+		| head -1 \
+		| awk -F\= '{print $2}' \
+		| sed -E 's/[[:space:]]*\"([^[:space:]]*)\"[[:space:]]*$/\1/g'
 }
 
-stage() {
-	info "Creating staging directory..."
-	mkdir -p ${STAGEDIR}
+VERSION=$(findversion)
 
-	star -copy -find . -type f \
-		 ! -path .git/\* \
-		 ! -name .gitignore \
-		 ! -name \*.o \
-		 ! -name \*.d \
-		 ! -name \*~ \
-		 ! -name gcli \
-		 ! -name \*.a \
-		 ! -name todo.org \
-		 ! -name config.mk \
-		 ! -name TAGS \
-		 ! -name \*.gz \
-		 ! -path tools/\* \
-		 ! -name y.tab.h \
-		 ${STAGEDIR}
-}
+DIR=/tmp/gcli-${VERSION}
+mkdir -p $DIR
 
+echo "Making BZIP tarball"
+git archive --format=tar --prefix=gcli-$VERSION/ v$VERSION \
+	| bzip2 -v > $DIR/gcli-$VERSION.tar.bz2
+echo "Making XZ tarball"
+git archive --format=tar --prefix=gcli-$VERSION/ v$VERSION \
+	| xz -v > $DIR/gcli-$VERSION.tar.xz
+echo "Making GZIP tarball"
+git archive --format=tar --prefix=gcli-$VERSION/ v$VERSION \
+	| gzip -v > $DIR/gcli-$VERSION.tar.gz
 
+OLDDIR=$(pwd)
+cd $DIR
+echo "Calculating SHA256SUMS"
+sha256sum *.tar* > SHA256SUMS
+cd $OLDDIR
 
-NAMEBASE="gcli-`findversion`"
-TARFBASE="${NAMEBASE}.tar"
-STAGEDIR="/tmp/${NAMEBASE}/"
-
-if [ ! -f Makefile ]; then
-	die "Please change to the source top"
-fi
-
-info "Staging into ${STAGEDIR} ..."
-stage
-
-info "Creating base tarball ${TARFBASE} ..."
-pushd /tmp > /dev/null 2>&1
-
-star -c -f "${TARFBASE}" "${NAMEBASE}"
-
-info "Compressing tarballs..."
-xz < "${TARFBASE}" > "${TARFBASE}".xz
-gzip < "${TARFBASE}" > "${TARFBASE}".gz
-bzip2 < "${TARFBASE}" > "${TARFBASE}".bz2
-
-info "Shuffling around output files..."
-
-rm "${TARFBASE}"
-rm -rf "${STAGEDIR}"
-mkdir -p "${STAGEDIR}"
-
-mv "${TARFBASE}".* "${STAGEDIR}"
-
-popd > /dev/null 2>&1
-
-info "Tarballs are at ${STAGEDIR}"
+echo "Release Tarballs are at $DIR"

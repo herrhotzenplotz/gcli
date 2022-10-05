b4b0337d9256afdb6ac325b8f0cff4b3e7b8bfc5
Nico Sonack <nsonack@herrhotzenplotz.de>
2022-10-05 17:43:18 +0200

    (#147) Convert Gitlab Labels to generated parsers

--- src/gitlab/labels.c
+++ src/gitlab/labels.c
@@ -31,46 +31,9 @@
 #include <gcli/gitlab/labels.h>
 #include <gcli/json_util.h>
 
+#include <templates/gitlab/labels.h>
+
 #include <pdjson/pdjson.h>
-
-static uint32_t
-gitlab_get_color(struct json_stream *input)
-{
-    char *color  = get_string(input);
-    char *endptr = NULL;
-    long  code   = 0;
-
-    code = strtol(color + 1, &endptr, 16);
-    if (endptr != (color + 1 + strlen(color + 1)))
-        err(1, "error: color code is invalid");
-
-    free(color);
-
-    return ((uint32_t)(code) << 8);
-}
-
-static void
-gitlab_parse_label(struct json_stream *input, gcli_label *it)
-{
-    if (json_next(input) != JSON_OBJECT)
-        errx(1, "expected label object");
-
-    while (json_next(input) == JSON_STRING) {
-        size_t      len = 0;
-        const char *key = json_get_string(input, &len);
-
-        if (strncmp("name", key, len) == 0)
-            it->name = get_string(input);
-        else if (strncmp("description", key, len) == 0)
-            it->description = get_string(input);
-        else if (strncmp("color", key, len) == 0)
-            it->color = gitlab_get_color(input);
-        else if (strncmp("id", key, len) == 0)
-            it->id = get_int(input);
-        else
-            SKIP_OBJECT_VALUE(input);
-    }
-}
 
 size_t
 gitlab_get_labels(
@@ -96,19 +59,8 @@ gitlab_get_labels(
         gcli_fetch(url, &next_url, &buffer);
 
         json_open_buffer(&stream, buffer.data, buffer.length);
-        json_set_streaming(&stream, 1);
 
-        next_type = json_next(&stream);
-        if (next_type != JSON_ARRAY)
-            errx(1, "error: expected array of labels");
-
-        while (json_peek(&stream) != JSON_ARRAY_END) {
-            gcli_label *it;
-
-            *out = realloc(*out, sizeof(gcli_label) * (out_size + 1));
-            it = &(*out)[out_size++];
-            gitlab_parse_label(&stream, it);
-        }
+        parse_gitlab_labels(&stream, out, &out_size);
 
         free(buffer.data);
         free(url);
@@ -148,7 +100,7 @@ gitlab_create_label(const char *owner, const char *repo, gcli_label *label)
     json_open_buffer(&stream, buffer.data, buffer.length);
     json_set_streaming(&stream, 1);
 
-    gitlab_parse_label(&stream, label);
+    parse_gitlab_label(&stream, label);
 
     json_close(&stream);
     free(lname_escaped.data);

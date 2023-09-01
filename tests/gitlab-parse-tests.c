fe3bf8494d7daf6da0ec86cb8998bc8beb5cafce
Nico Sonack <nsonack@herrhotzenplotz.de>
2023-09-01 19:54:04 +0200

    Add test for Gitlab Merge Request Parser
    While we're at it, fix the missing entry for the head_pipeline_id.

--- tests/gitlab-parse-tests.c
+++ tests/gitlab-parse-tests.c
@@ -1,6 +1,4 @@
-#include <templates/github/issues.h>
-#include <templates/github/pulls.h>
-#include <templates/github/labels.h>
+#include <templates/gitlab/merge_requests.h>
 
 #include <err.h>
 #include <string.h>
@@ -42,11 +40,37 @@ open_sample(char const *const name)
 ATF_TC_WITHOUT_HEAD(gitlab_simple_merge_request);
 ATF_TC_BODY(gitlab_simple_merge_request, tc)
 {
+	json_stream stream = {0};
 	gcli_ctx *ctx = test_context();
 	FILE *f = open_sample("gitlab_simple_merge_request.json");
+	gcli_pull pull = {0};
 
-	(void) ctx;
-	(void) f;
+	json_open_stream(&stream, f);
+	ATF_REQUIRE(parse_gitlab_mr(ctx, &stream, &pull) == 0);
+
+	ATF_CHECK_STREQ(pull.author, "herrhotzenplotz");
+	ATF_CHECK_STREQ(pull.state, "merged");
+	ATF_CHECK_STREQ(pull.title, "Fix test suite");
+	ATF_CHECK_STREQ(pull.body, "This finally fixes the broken test suite");
+	ATF_CHECK_STREQ(pull.created_at, "2023-08-31T23:37:50.848Z");
+	ATF_CHECK(pull.commits_link == NULL);
+	ATF_CHECK_STREQ(pull.head_label, "fix-test-suite");
+	ATF_CHECK_STREQ(pull.base_label, "trunk");
+	ATF_CHECK_STREQ(pull.head_sha, "3eab596a6806434e4a34bb19de12307ab1217af3");
+	ATF_CHECK(pull.milestone == NULL);
+	ATF_CHECK(pull.id == 246912053);
+	ATF_CHECK(pull.number == 216);
+	ATF_CHECK(pull.comments == 0); // not supported
+	ATF_CHECK(pull.additions == 0); // not supported
+	ATF_CHECK(pull.deletions == 0); // not supported
+	ATF_CHECK(pull.commits == 0); // not supported
+	ATF_CHECK(pull.changed_files == 0); // not supported
+	ATF_CHECK(pull.head_pipeline_id == 989409992);
+	ATF_CHECK(pull.labels_size == 0);
+	ATF_CHECK(pull.labels == NULL);
+	ATF_CHECK(pull.merged == false);
+	ATF_CHECK(pull.mergeable == true);
+	ATF_CHECK(pull.draft == false);
 }
 
 ATF_TP_ADD_TCS(tp)

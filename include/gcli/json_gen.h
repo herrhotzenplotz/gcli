38605cad32f991493cfc2d895ab19364d76e12fc
Nico Sonack <nsonack@herrhotzenplotz.de>
2023-09-17 17:49:23 +0200

    (#189) Add ground work for json generator

---
+++ include/gcli/json_gen.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 Nico Sonack <nsonack@herrhotzenplotz.de>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GCLI_JSON_GEN_H
+#define GCLI_JSON_GEN_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+enum {
+	GCLI_JSONGEN_ARRAY,
+	GCLI_JSONGEN_OBJECT,
+};
+
+typedef struct gcli_jsongen gcli_jsongen;
+struct gcli_jsongen {
+	char *buffer;
+	size_t buffer_size;
+	size_t buffer_capacity;
+
+	int scopes[32];           /* scope stack */
+	size_t scopes_size;       /* scope stack pointer */
+};
+
+int gcli_jsongen_init(gcli_jsongen *gen);
+void gcli_jsongen_free(gcli_jsongen *gen);
+
+#endif /* GCLI_JSON_GEN_H */

402c4ed3d8c0ed331be171cabfa4a9399a25954d
Gavin-John Noonan <mail@gjnoonan.co.uk>
2023-09-27 16:40:27 +0100

    feat(docs/tutorial): generate next/previous links for navigation
    When generating a page, we now add next/previous links to both the header and
    the footer, to help with navigating between tutorial pages.
    
    I would have likes to stick with just using sh, however there was no clean
    solution so AWK has been used to keep track of the next/previous page as well as
    the page title. However the script remains POSIX compliant.

--- docs/tutorial/gen.sh
+++ docs/tutorial/gen.sh
@@ -11,54 +11,97 @@ set -eu
 #
 
 header() {
-    title="${1}"
-    sed "s/{{TITLE_PLACEHOLDER}}/${title}/g" top.html
+	TITLE="${1}"
+	sed "s/{{TITLE_PLACEHOLDER}}/${title}/g" top.html
 }
 
 footer() {
-    cat footer.html
+	cat footer.html
+}
+
+pagination() {
+	PREVDOC="$1"
+	PREVTITLE="$2"
+	NEXTDOC="$3"
+	NEXTTITLE="$4"
+
+	echo "<nav style=text-align:right>"
+	if [ -n "${PREVDOC}" ]; then
+		echo "<a href=\"${PREVDOC}\" title=\"${PREVTITLE}\">⇐ Previous</a>"
+	fi
+
+	echo "<a href=\"index.html\">Table of contents</a>"
+
+	if [ -n "${NEXTDOC}" ]; then
+		echo "<a href=\"${NEXTDOC}\" title=\"${NEXTTITLE}\">Next ⇒</a>"
+	fi
+	echo "</nav>"
 }
 
 genindex() {
-    header "Index"
+	header "Index"
 
-    cat <<EOF
-    <h1>A GCLI Tutorial</h1>
-    <p>This document is aimed at those who are new to gcli and want get
-    started using it.</p>
+	cat <<EOF
+	<h1>A GCLI Tutorial</h1>
+	<p>This document is aimed at those who are new to gcli and want get
+	started using it.</p>
 
-    <h2>Table of contents</h2>
+	<h2>Table of contents</h2>
 EOF
 
-    echo "<ol>"
+	echo "<ol>"
 
-    awk -F\\t '{printf "<li><a href=\"./%s\">%s</a></li>\n", $1, $2}' < toc
+	awk -F\\t '{printf "<li><a href=\"./%s\">%s</a></li>\n", $1, $2}' < toc
 
-    echo "</ol>"
+	echo "</ol>"
 
-    footer
+	footer
 }
 
 genpage() {
-    PAGETITLE="${1}"
-    PAGEMDFILE="${2}"
+	PAGETITLE="$1"
+	PAGEMDFILE="$2"
+	PREVDOC="$3"
+	PREVTITLE="$4"
+	NEXTDOC="$5"
+	NEXTTITLE="$6"
 
-    header "${PAGETITLE}"
+	header "${PAGETITLE}"
 
-    echo "<p><a href=\"index.html\">⇐ Back to table of contents</a></p>"
-    echo "<hr />"
-    cmark -t html < "${PAGEMDFILE}"
-    echo "<br />"
-    echo "<hr />"
-    echo "<p><a href=\"index.html\">⇐ Back to table of contents</a></p>"
+	pagination "${PREVDOC}" "${PREVTITLE}" "${NEXTDOC}" "${NEXTTITLE}"
+	echo "<hr />"
 
-    footer
+
+	cmark -t html < "${PAGEMDFILE}"
+	echo "<br />"
+	echo "<hr />"
+
+	pagination "${PREVDOC}" "${PREVTITLE}" "${NEXTDOC}" "${NEXTTITLE}"
+
+	footer
 }
+
+prevhtmldoc=""
+prevtitlename=""
+while IFS="$(printf '\t')" read -r htmldoc title; do
+	mddoc="${htmldoc%.html}.md"
+
+
+	read -r nexthtmldoc nexttitle <<EOF
+$(awk -F'\t' -v current="$htmldoc" 'BEGIN{flag=0} {if(flag==1){print $1 "\t" $2; exit} if($1==current){flag=1}}' toc)
+EOF
+
+	if [ -n "$prevhtmldoc" ]; then
+	prevtitlename=$(awk -F'\t' -v current="$prevhtmldoc" '{if($1==current){print $2; exit}}' toc)
+	fi
+
+	# Generating the pages
+	genpage "${title}" "${mddoc}" "${prevhtmldoc}" "${prevtitlename}" "${nexthtmldoc}" "${nexttitle}" > "${htmldoc}"
+
+	# Update the previous document filename and title for the next iteration
+	prevhtmldoc="$htmldoc"
+	prevtitlename="$title"
+done < toc
 
 genindex > index.html
 
-while IFS="$(printf '\t')" read -r htmldoc title; do
-    mddoc="${htmldoc%.html}.md"
-
-    genpage "${title}" "${mddoc}" > "${htmldoc}"
-done < toc

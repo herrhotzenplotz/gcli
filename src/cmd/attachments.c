e125a28d8f87d6d8be7768767305a27067f55517
Nico Sonack <nsonack@herrhotzenplotz.de>
2023-12-15 16:27:44 +0100

    Add attachments subcommand argument parsing logic
    This is very simple for now. There's only one 'get' subcommand and
    not a lot of options. We will gradually expand these in the future
    depending on what we need from this subcommand.
    
    I am unsure whether adding a new subcommand is the right way to go
    here but I cannot think of a better way of interfacing with attachments
    right now since they are living in their own ID namespace.

--- src/cmd/attachments.c
+++ src/cmd/attachments.c
@@ -30,13 +30,113 @@
 #include <gcli/gcli.h>
 #include <gcli/cmd/cmd.h>
 
-int
-subcommand_attachments(int argc, char *argv[])
+#include <getopt.h>
+#include <stdlib.h>
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: gcli [options] attachments -i <id> actions...\n");
+	fprintf(stderr, "OPTIONS:\n");
+	fprintf(stderr, "  -i id       Execute the given actions for the specified attachment id.\n");
+	fprintf(stderr, "ACTIONS:\n");
+	fprintf(stderr, "  get [-o path]  Fetch and dump the contents of the "
+	                  "attachments to the given path or stdout\n");
+	fprintf(stderr, "\n");
+	version();
+}
+
+static int
+action_attachment_get(int *argc, char ***argv, gcli_id const id)
 {
 	(void) argc;
 	(void) argv;
+	(void) id;
 
-	fprintf(stderr, "gcli: attachments subcommand is not yet implemented\n");
+	fprintf(stderr, "gcli: get action is not yet implemented\n");
+	return EXIT_FAILURE;
+}
 
-	return 1;
+static struct action {
+	char const *const name;
+	int (*fn)(int *argc, char ***argv, gcli_id const id);
+} const actions[] = {
+	{ .name = "get", .fn = action_attachment_get },
+};
+
+static size_t const actions_size = ARRAY_SIZE(actions);
+
+static struct action const *
+find_action(char const *const name)
+{
+	for (size_t i = 0; i < actions_size; ++i) {
+		if (strcmp(name, actions[i].name) == 0)
+			return &actions[i];
+	}
+	return NULL;
+}
+
+int
+subcommand_attachments(int argc, char *argv[])
+{
+	int ch;
+	gcli_id iflag;
+	bool iflag_seen = false;
+
+	struct option options[] = {
+		{ .name = "id", .has_arg = required_argument, .flag = NULL, .val = 'i' },
+		{0},
+	};
+
+	while ((ch = getopt_long(argc, argv, "+i:", options, NULL)) != -1) {
+		switch (ch) {
+		case 'i': {
+			char *endptr;
+
+			iflag_seen = true;
+			iflag = strtoull(optarg, &endptr, 10);
+
+			if (optarg + strlen(optarg) != endptr) {
+				fprintf(stderr, "gcli: bad attachment id »%s«\n", optarg);
+				return EXIT_FAILURE;
+			}
+		} break;
+		default:
+			usage();
+			return EXIT_FAILURE;
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (!iflag_seen) {
+		fprintf(stderr, "gcli: missing -i flag\n");
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (argc == 0) {
+		fprintf(stderr, "gcli: missing actions\n");
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	while (argc) {
+		int rc;
+		char const *const action_name = *argv;
+		struct action const *const action = find_action(action_name);
+
+		if (action == NULL) {
+			fprintf(stderr, "gcli: %s: no such action\n", action_name);
+			usage();
+			return EXIT_FAILURE;
+		}
+
+		rc = action->fn(&argc, &argv, iflag);
+		if (rc)
+			return rc;
+	}
+
+	return 0;
 }

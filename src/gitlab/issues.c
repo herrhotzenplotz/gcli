a5b1aecd029abcec2c5688cf6031326f2a6998a5
Nico Sonack <nsonack@herrhotzenplotz.de>
2025-06-01 10:27:14 +0000

    gitlab_issues_search: convert to using new gcli_url_options_append routines
    Again, great simplification of the code!
    
    Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

--- src/gitlab/issues.c
+++ src/gitlab/issues.c
@@ -34,6 +34,7 @@
 #include <gcli/gitlab/repos.h>
 #include <gcli/json_gen.h>
 #include <gcli/json_util.h>
+#include <gcli/url.h>
 
 #include <templates/gitlab/issues.h>
 
@@ -110,7 +111,8 @@ gitlab_issues_make_url(struct gcli_ctx *ctx, struct gcli_path const *const path,
 {
 	int rc = 0;
 
-	rc = gitlab_repo_make_url(ctx, path, url, "/issues%s", suffix);
+	rc = gitlab_repo_make_url(ctx, path, url, "/issues%s",
+	                          suffix ? suffix : "");
 
 	return rc;
 }
@@ -120,67 +122,15 @@ gitlab_issues_search(struct gcli_ctx *ctx, struct gcli_path const *const path,
                      struct gcli_issue_fetch_details const *details,
                      int const max, struct gcli_issue_list *const out)
 {
-	char *url = NULL, *e_author = NULL, *e_labels = NULL,
-	     *e_milestone = NULL, *e_search = NULL, *suffix = NULL,
-	     *e_assignee = NULL, separator = '?';
+	char *url = NULL, *suffix = NULL;
 	int rc = 0;
 
-	if (!details->all)
-		separator = '&';
-
-	if (details->author) {
-		char *tmp = gcli_urlencode(details->author);
-
-		e_author = gcli_asprintf("%cauthor_username=%s", separator, tmp);
-		gcli_clear_ptr(&tmp);
-		separator = '&';
-	}
-
-	if (details->label) {
-		char *tmp = gcli_urlencode(details->label);
-
-		e_labels = gcli_asprintf("%clabels=%s", separator, tmp);
-		gcli_clear_ptr(&tmp);
-		separator = '&';
-	}
-
-	if (details->milestone) {
-		char *tmp = gcli_urlencode(details->milestone);
-
-		e_milestone = gcli_asprintf("%cmilestone=%s", separator, tmp);
-		gcli_clear_ptr(&tmp);
-		separator = '&';
-	}
-
-	if (details->assignee) {
-		char *tmp = gcli_urlencode(details->assignee);
-
-		e_assignee = gcli_asprintf("%cassignee_username=%s", separator, tmp);
-		gcli_clear_ptr(&tmp);
-		separator = '&';
-	}
-
-	if (details->search_term) {
-		char *tmp = gcli_urlencode(details->search_term);
-
-		e_search = gcli_asprintf("%csearch=%s", separator, tmp);
-		gcli_clear_ptr(&tmp);
-		separator = '&';
-	}
-
-	suffix = gcli_asprintf("%s%s%s%s%s%s",
-	                       details->all ? "" : "?state=opened",
-	                       e_author ? e_author : "",
-	                       e_labels ? e_labels : "",
-	                       e_milestone ? e_milestone : "",
-	                       e_assignee ? e_assignee : "",
-	                       e_search ? e_search : "");
-
-	gcli_clear_ptr(&e_milestone);
-	gcli_clear_ptr(&e_author);
-	gcli_clear_ptr(&e_labels);
-	gcli_clear_ptr(&e_assignee);
-	gcli_clear_ptr(&e_search);
+	gcli_url_options_append(&suffix, "state", details->all ? NULL : "opened");
+	gcli_url_options_append(&suffix, "author_username", details->author);
+	gcli_url_options_append(&suffix, "labels", details->label);
+	gcli_url_options_append(&suffix, "milestone", details->milestone);
+	gcli_url_options_append(&suffix, "assignee_username", details->assignee);
+	gcli_url_options_append(&suffix, "search", details->search_term);
 
 	rc = gitlab_issues_make_url(ctx, path, suffix, &url);
 

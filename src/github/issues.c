d8c8316aad4ade81a448ee07299735a1c57bc1e0
Nico Sonack <nsonack@herrhotzenplotz.de>
2025-06-01 10:27:14 +0000

    github issues: convert URL generator to new options helper routine
    This simplifies the code a lot! Which is really nice!
    
    Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

--- src/github/issues.c
+++ src/github/issues.c
@@ -33,6 +33,7 @@
 #include <gcli/github/milestones.h>
 #include <gcli/json_gen.h>
 #include <gcli/json_util.h>
+#include <gcli/url.h>
 #include <pdjson/pdjson.h>
 
 #include <templates/github/issues.h>
@@ -249,12 +250,12 @@ github_issues_make_url(struct gcli_ctx *ctx, struct gcli_path const *const path,
 		e_repo = gcli_urlencode(path->as_default.repo);
 		*out = gcli_asprintf("%s/repos/%s/%s/issues%s",
 		                     gcli_get_apibase(ctx),
-		                     e_owner, e_repo, suffix);
+		                     e_owner, e_repo, suffix ? suffix : "");
 		gcli_clear_ptr(&e_owner);
 		gcli_clear_ptr(&e_repo);
 	} break;
 	case GCLI_PATH_URL: {
-		*out = gcli_asprintf("%s%s", path->as_url, suffix);
+		*out = gcli_asprintf("%s%s", path->as_url, suffix ? suffix : "");
 	} break;
 	default: {
 		rc = gcli_error(ctx, "unsupported path kind for issue list");
@@ -270,8 +271,7 @@ get_issues(struct gcli_ctx *ctx, struct gcli_path const *const path,
            struct gcli_issue_fetch_details const *details, int const max,
            struct gcli_issue_list *const out)
 {
-	char *url = NULL, *e_author = NULL, *e_label = NULL,
-	     *e_milestone = NULL, *e_assignee = NULL, *suffix = NULL;
+	char *url = NULL, *suffix = NULL;
 	int rc = 0;
 
 	if (details->milestone) {
@@ -281,41 +281,16 @@ get_issues(struct gcli_ctx *ctx, struct gcli_path const *const path,
 		if (rc < 0)
 			return rc;
 
-		e_milestone = gcli_asprintf("&milestone=%"PRIid, milestone_id);
+		gcli_url_options_appendf(&suffix, "milestone", "%"PRIid, milestone_id);
 	}
 
-	if (details->author) {
-		char *tmp = gcli_urlencode(details->author);
-		e_author = gcli_asprintf("&creator=%s", tmp);
-		gcli_clear_ptr(&tmp);
-	}
-
-	if (details->label) {
-		char *tmp = gcli_urlencode(details->label);
-		e_label = gcli_asprintf("&labels=%s", tmp);
-		gcli_clear_ptr(&tmp);
-	}
-
-	if (details->assignee) {
-		char *tmp = gcli_urlencode(details->assignee);
-		e_assignee = gcli_asprintf("&assignee=%s", tmp);
-		gcli_clear_ptr(&tmp);
-	}
-
-	suffix = gcli_asprintf(
-		"?state=%s%s%s%s%s",
-		details->all ? "all" : "open",
-		e_author ? e_author : "",
-		e_label ? e_label : "",
-		e_assignee ? e_assignee : "",
-		e_milestone ? e_milestone : "");
+	gcli_url_options_append(&suffix, "creator", details->author);
+	gcli_url_options_append(&suffix, "labels", details->label);
+	gcli_url_options_append(&suffix, "assignee", details->assignee);
+	gcli_url_options_append(&suffix, "state", details->all ? "all" : "open");
 
 	rc = github_issues_make_url(ctx, path, suffix, &url);
 
-	gcli_clear_ptr(&e_milestone);
-	gcli_clear_ptr(&e_author);
-	gcli_clear_ptr(&e_label);
-	gcli_clear_ptr(&e_assignee);
 	gcli_clear_ptr(&suffix);
 
 	if (rc < 0)

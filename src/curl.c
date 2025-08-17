540e4eb9f2145700b98064019c80fa779abe0af1
Nico Sonack <nsonack@herrhotzenplotz.de>
2025-08-17 19:17:27 +0200

    curl.c: fix prototype of xferinfo_cb causing build failures on sparc64-netbsd
    This was discovered by:
    
    	Target: sparc64--netbsd
    	Thread model: posix
    	Supported LTO compression algorithms: zlib
    	gcc version 10.5.0 (nb3 20231008)
    
    I have NO idea why it was never discovered by any modern gcc on
    plain-old amd64 hardware...  Makes me wonder :D
    
    Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

--- src/curl.c
+++ src/curl.c
@@ -148,8 +148,8 @@ gcli_fetch(struct gcli_ctx *ctx, char const *url, char **const pagination_next,
 }
 
 static int
-gcli_report_progress(void *_ctx, double dltotal, double dlnow,
-                     double ultotal, double ulnow)
+gcli_report_progress(void *_ctx, curl_off_t dltotal, curl_off_t dlnow,
+                     curl_off_t ultotal, curl_off_t ulnow)
 {
 	struct gcli_ctx *ctx = _ctx;
 

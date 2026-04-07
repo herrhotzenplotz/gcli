a337ec060e8fa31c33b2c726e6a2c81e806330c6
Nico Sonack <nsonack@herrhotzenplotz.de>
2026-04-07 07:05:41 +0000

    github: implement get_pipeline
    This allows the 'status' action to work now.

--- include/gcli/github/checks.h
+++ include/gcli/github/checks.h
@@ -45,6 +45,10 @@ int github_get_check_suites(struct gcli_ctx *ctx,
                             struct gcli_pipelines_fetch_details const *details,
                             struct gcli_pipeline_list *out);
 
+int github_get_check_suite(struct gcli_ctx *ctx,
+                           struct gcli_path const *pipeline_path,
+                           struct gcli_pipeline *out);
+
 int github_get_check_runs(struct gcli_ctx *ctx,
                           struct gcli_path const *pipeline_path,
                           int count,
